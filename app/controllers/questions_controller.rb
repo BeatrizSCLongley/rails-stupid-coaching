e8842b0cc4f2bbbc10c99ddd334c3bebffa34e73
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-04-07 09:38:42 +0200

    finished and tests added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,6 @@
 class QuestionsController < ApplicationController
   # Needs to read the question from params and compute an instance variable @answer for the view to display
   def ask
-    # Should handle two requests:
-    # /answer?question=hello
-    # /answer?question=what time is it?
   end
 
   def answer
@@ -12,5 +9,15 @@ class QuestionsController < ApplicationController
     # If I then call params[:question] I get 'something'
     @question = params[:question]
     @time_answer = "The time is #{Time.now}"
+
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?('?')
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
